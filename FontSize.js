f5bd490e277bc2a7fb1c892c9c1286a3bb279d32
Champ-Higdon <champhigdon3@gmail.com>
2025-08-10 13:27:28 -0400

    Added font changer to home page

---
+++ FontSize.js
@@ -0,0 +1,15 @@
+
+
+
+document.getElementById('font-size-1').addEventListener('click', function() {
+    console.log('Font size 1 clicked!');
+    // Or do something like change font size
+});
+
+document.getElementById('font-size-2').addEventListener('click', function() {
+    console.log('Font size 2 clicked!');
+});
+
+document.getElementById('font-size-3').addEventListener('click', function() {
+    console.log('Font size 3 clicked!');
+});
